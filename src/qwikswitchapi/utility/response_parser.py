6b8bf6e55c3ae1d212edfb1275245b7185fc8242
Riaan Hanekom <riaan@hanekom.io>
2025-01-04 20:26:15 +0200

    Documentation updates

--- src/qwikswitchapi/utility/response_parser.py
+++ src/qwikswitchapi/utility/response_parser.py
@@ -6,13 +6,29 @@ class ResponseParser:
 
     @staticmethod
     def get_failure_message(resp) -> str:
+        """
+        Returns a formatted message indicating the request failed
+        :param resp: The response object
+        :return: A formatted message indicating the request failed
+        """
         request_url = resp.request.url
         return f'Failed to call {request_url}.  Status code: "{resp.status_code}", body: "{resp.text}"'
 
     @staticmethod
     def raise_request_failure(url, ex: RequestException):
+        """
+        Raises a QSException indicating the request failed
+        :param url: The URL of the request
+        :param ex: The exception that was raised
+        :raises QSException: Indicating the request failed, chained with the original exception.
+        """
         raise QSException(f'Request to {url} failed: {str(ex)}') from ex
 
     @staticmethod
     def raise_request_error(resp):
+        """
+        Raises a QSException indicating the request failed
+        :param resp: The response object
+        :raises QSException: Indicating the request failed, with the body of the response.
+        """
         raise QSException(ResponseParser.get_failure_message(resp))
--- src/qwikswitchapi/utility/url_builder.py
+++ src/qwikswitchapi/utility/url_builder.py
@@ -4,14 +4,33 @@ class UrlBuilder:
 
     @staticmethod
     def build_get_all_device_status_url(base_uri: str, key: str) -> str:
+        """
+        Builds the URL to get the status of all devices
+        :param base_uri: The base URI to use
+        :param key: The API key to use
+        :return: The url to the 'Get all device status' endpoint
+        """
         return urljoin(base_uri, f'state/{quote_plus(key)}/')
 
     @staticmethod
     def build_generate_api_keys_url(base_uri: str) -> str:
+        """
+        Builds the URL to generate API keys
+        :param base_uri: The base URI to use
+        :return: The url to the 'Generate API keys' endpoint
+        """
         return urljoin(base_uri, 'keys')
 
     @staticmethod
-    def build_control_url(base_uri: str, key: str, device: str, level: int) -> str:
+    def build_control_url(base_uri: str, key: str, device_id: str, level: int) -> str:
+        """
+        Builds the URL to control a device
+        :param base_uri: The base URI to use
+        :param key: The API key to use
+        :param device_id: The unique identifier of the device to control
+        :param level: The level to set the device to
+        :return: The url to the `Control a device` endpoint
+        """
         return urljoin(base_uri,
                        f'control/{quote_plus(key)}/' +
-                       f'?device={quote_plus(device)}&setlevel={level}')
+                       f'?device={quote_plus(device_id)}&setlevel={level}')
